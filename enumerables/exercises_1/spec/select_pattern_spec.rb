b9a60ccbb9bd1ee776cd91e0176d1bc9e1f12f41
Jason Gong <mhpjay@gmail.com>
2021-03-29 18:26:38 -0400

    complete select_pattern_spec.rb

--- enumerables/exercises_1/spec/select_pattern_spec.rb
+++ enumerables/exercises_1/spec/select_pattern_spec.rb
@@ -9,67 +9,68 @@ RSpec.describe 'select pattern' do
     expect(evens).to eq([2, 4, 6, 8, 10])
   end
 
-  xit 'picks odd numbers' do
+  it 'picks odd numbers' do
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     odds = []
     numbers.each do |number|
-      # Your code goes here
+      odds << number if number.odd?
     end
     expect(odds).to eq([1, 3, 5, 7, 9])
   end
 
-  xit 'words with three letters' do
+  it 'words with three letters' do
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
     selected = []
-    # Your code goes here
+    words.each {|word| selected << word if word.length == 3}
     expect(selected).to eq(["bad", "cat", "dog", "red"])
   end
 
-  xit 'words with more than three letters' do
+  it 'words with more than three letters' do
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
-    # Your code goes here
+    selected = words.select {|word| word.length > 3}
     expect(selected).to eq(["pill", "finger", "blue", "table"])
   end
 
-  xit 'words ending in e' do
+  it 'words ending in e' do
     words = ["are", "you", "strike", "thinking", "belt", "piece", "warble", "sing", "pipe"]
-    # Your code goes here
+    selected = words.select {|word| word[-1] == "e"}
     expect(selected).to eq(["are", "strike", "piece", "warble", "pipe"])
   end
 
-  xit 'words ending in ing' do
+  it 'words ending in ing' do
     words = ["bring", "finger", "drought", "singing", "bingo", "purposeful"]
-    # Your code goes here
+    selected = words.select {|word| word[-3..-1] == "ing"}
     expect(selected).to eq(["bring", "singing"])
   end
 
-  xit 'words containing e' do
+  it 'words containing e' do
     words = ["four", "red", "five", "blue", "pizza", "purple"]
-    # Your code goes here
+    selected = words.select {|word| word.include?("e")}
     expect(selected).to eq(["red", "five", "blue", "purple"])
   end
 
-  xit 'dinosaurs' do
+  it 'dinosaurs' do
     animals = ["tyrannosaurus", "narwhal", "eel", "achillesaurus", "qingxiusaurus"]
-    # Your code goes here
+    dinosaurs = animals.select {|ani| ani.include?("saurus")}
     expect(dinosaurs).to eq(["tyrannosaurus", "achillesaurus", "qingxiusaurus"])
   end
 
-  xit 'floats' do
+  it 'floats' do
     numbers = [3, 1.4, 3.5, 2, 4.9, 9.1, 8.0]
-    # Your code goes here
+    floats = numbers.select{|num| num.integer? == false}
     expect(floats).to eq([1.4, 3.5, 4.9, 9.1, 8.0])
   end
 
-  xit 'arrays' do
+  it 'arrays' do
     elements = ["CAT", ["dog"], 23, [56, 3, 8], "AIMLESS", 43, "butter"]
-    # Your code goes here
+    # require "pry"; binding.pry
+    arrays = elements.select {|ele| ele.class == Array}
     expect(arrays).to eq([["dog"], [56, 3, 8]])
   end
 
-  xit 'hashes' do
+  it 'hashes' do
     elements = ["cat", {:dog=>"fido"}, 23, {:stuff=>"things"}, "aimless", 43]
-    # Your code goes here
+    hashes = elements.select {|ele| ele.class == Hash}
     expect(hashes).to eq([{:dog=>"fido"}, {:stuff=>"things"}])
   end
 end
