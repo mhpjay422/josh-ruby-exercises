6a035f566916c74d224479c380343a2d2a0d57f5
Jason Gong <mhpjay@gmail.com>
2021-03-30 11:36:46 -0400

    complete wizard_spec.rb

---
+++ mythical-creatures/lib/wizard.rb
@@ -0,0 +1,25 @@
+class Wizard
+  attr_reader :name, :bearded, :rested
+  def initialize(name, bearded={bearded: true}, energy=3)
+    @name = name
+    @bearded = bearded
+    @energy = energy
+  end
+
+  def incantation(spell)
+    "sudo #{spell}"
+  end
+
+  def bearded?
+    @bearded[:bearded]
+  end
+
+  def rested?
+    @energy > 0    
+  end
+  
+  def cast
+    @energy -= 1
+    "MAGIC MISSLE!"
+  end
+end
--- mythical-creatures/spec/wizard_spec.rb
+++ mythical-creatures/spec/wizard_spec.rb
@@ -1,5 +1,6 @@
+require './spec_helper'
 require 'rspec'
-require './lib/wizard'
+require '../lib/wizard'
 
 RSpec.describe Wizard do
   it 'has a name' do
@@ -34,19 +35,29 @@ RSpec.describe Wizard do
 
   it 'starts rested' do
     # create wizard
+    wiz = Wizard.new("Gandalf")
     # .rested? returns true
+    expect(wiz.rested?).to eq true 
   end
 
   it 'can cast spells' do
     # create wizard
+    wiz = Wizard.new("Merlin")
     # .cast returns "MAGIC MISSILE!"
+    expect(wiz.cast).to eq "MAGIC MISSLE!"
   end
 
   it 'gets tired after casting three spells' do
     # create wizard
+    wiz = Wizard.new("Jaygo")
     # casts spell twice
+    wiz.cast
+    wiz.cast
     # check if wizard is rested
+    expect(wiz.rested?).to be true
     # casts spell
+    wiz.cast
     # check wizard is not rested
+    expect(wiz.rested?).to be false
   end
 end
