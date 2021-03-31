5981e2f25037baf3f6c97d0681cb23d6ab9bcf85
Jason Gong <mhpjay@gmail.com>
2021-03-31 18:52:37 -0400

    refactor wizard

--- mythical-creatures/lib/wizard.rb
+++ mythical-creatures/lib/wizard.rb
@@ -1,39 +1,13 @@
-# class Wizard
-#   attr_reader :name, :bearded, :rested
-#   def initialize(name, bearded={bearded: true}, energy=3)
-#     @name = name
-#     @bearded = bearded
-#     @energy = energy
-#   end
-
-#   def incantation(spell)
-#     "sudo #{spell}"
-#   end
-
-#   def bearded?
-#     @bearded[:bearded]
-#   end
-
-#   def rested?
-#     @energy > 0    
-#   end
-  
-#   def cast
-#     @energy -= 1
-#     "MAGIC MISSLE!"
-#   end
-# end
-
 class Wizard
-  attr_reader :name, :bearded
-  def initialize(name, bearded={bearded: true}, energy=3)
+  attr_reader :name, :bearded, :energy
+  def initialize(name, bearded: true)
     @name = name
     @bearded = bearded
-    @energy = energy
+    @energy = 3
   end
 
   def bearded?
-    @bearded[:bearded]
+    bearded
   end
 
   def incantation(saying)
