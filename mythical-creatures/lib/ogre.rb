a4b862d478e7baf750aadb6d5de6c146b5dab277
Jason Gong <mhpjay@gmail.com>
2021-03-30 23:24:44 -0400

    complete ogre.rb

---
+++ mythical-creatures/lib/ogre.rb
@@ -0,0 +1,69 @@
+class Ogre
+  attr_reader :name, :home, :swing, :encounter
+  def initialize(name="Brak", home="Swamp", swing=0, encounter=0)
+    @name = name
+    @home = home
+    @swing = swing
+    @encounter = encounter
+  end
+
+  def encounter(person)
+    person.encounter
+    @encounter += 1
+    self.swing_at(person) if @encounter % 3 == 0
+  end
+
+  def encounter_counter
+    @encounter
+  end
+
+  def swing_at(person)
+    @swing += 1
+    person.knock_out if self.swings == 2
+  end
+
+  def swings  
+    @swing
+  end
+
+  def apologize(person)
+    person.wake_up
+  end
+end
+
+class Human 
+  attr_reader :name, :counter, :status
+  def initialize(name="Jane", counter=0, status="ok")
+    @name = name
+    @counter = counter
+    @status = status
+  end
+
+  def encounter
+    @counter += 1
+  end
+
+  def encounter_counter
+    @counter
+  end
+
+  def notices_ogre?
+    if @counter % 3 == 0
+      return true
+    else 
+      return false
+    end
+  end
+
+  def knock_out
+    @status = "knocked_out"
+  end
+
+  def knocked_out?
+    @status == "knocked_out"
+  end
+
+  def wake_up
+    @status = "ok"
+  end
+end
