2f52519fe371fff38c92e8fd479dbaee2174eda3
Jason Gong <mhpjay@gmail.com>
2021-03-31 11:06:45 -0400

    complete wallet_spec.rb

---
+++ command-query/exercises/wallet.rb
@@ -0,0 +1,30 @@
+class Wallet 
+  attr_reader :coin_pocket
+  def initialize(coin_pocket=[])
+    @coin_pocket = coin_pocket
+  end
+
+  def cents
+    return 0 if coin_pocket == []
+    coin_pocket.reduce(0) do |total, coin|
+      case 
+      when coin == :penny
+        total += 1
+      when coin == :nickel
+        total += 5
+      when coin == :dime
+        total += 10
+      when coin == :quarter
+        total += 25   
+      end
+    end
+  end
+
+  def <<(coin)
+    coin_pocket.push(coin)
+  end
+
+  def take(*coin)
+    coin.each { |c| coin_pocket.delete_at(coin_pocket.index(c) || coin_pocket.length)}
+  end
+end
