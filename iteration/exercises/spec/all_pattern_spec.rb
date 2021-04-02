8a256d85609468652cad5144d192ebf88bd49f13
Jason Gong <mhpjay@gmail.com>
2021-04-01 22:36:44 -0400

    complete iteration/all_pattern

--- iteration/exercises/spec/all_pattern_spec.rb
+++ iteration/exercises/spec/all_pattern_spec.rb
@@ -24,16 +24,16 @@ RSpec.describe 'all pattern' do
     expect(all_zeros).to be true
   end
 
-  xit 'test 3' do
+  it 'test 3' do
     words = ["love", "hate", "fire", "bird", "call"]
     all_four_letters = true
     words.each do |word|
-      # Your Code Here
+      word.length == 4
     end
     expect(all_four_letters).to be true
   end
 
-  xit 'test 4' do
+  it 'test 4' do
     words = {
       one: "love",
       two: "hate",
@@ -43,39 +43,40 @@ RSpec.describe 'all pattern' do
     }
     all_four_letters = true
     words.each do |position, word|
-      # Your Code Here
+      word.length == 4
     end
     expect(all_four_letters).to be true
   end
 
-  xit 'test 5' do
+  it 'test 5' do
     statuses = [:busy, :busy, :busy]
     all_busy = true
-    # Your Code Here
+    statuses.each {|stat| all_busy = false if stat != :busy}
 
     expect(all_busy).to be true
   end
 
-  xit 'test 6' do
+  it 'test 6' do
     friend_status = {
       "Megan" => :busy,
       "Sarah" => :busy,
       "Duncan" => :busy,
     }
     all_busy = true
-    # Your Code Here
+    friend_status.each {|k, status| all_busy = false if status != :busy}
 
     expect(all_busy).to be true
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     zip_codes = [94381, 831, 50009, 36232, 8992, 89999, 11110]
-    # Your Code Here
+    all_five_digits = true 
+    zip_codes.each {|zip| all_five_digits = false unless zip > 9999 && zip < 100000}
 
     expect(all_five_digits).to be false
   end
 
-  xit 'test 8' do
+  it 'test 8' do
     zip_codes = {
       "Megan" => 94381,
       "Sarah" => 831,
@@ -85,26 +86,28 @@ RSpec.describe 'all pattern' do
       "Cameron" => 89999,
       "Joshua" => 11110
     }
-    # Your Code Here
+    all_five_digits = true 
+    zip_codes.each {|k, zip| all_five_digits = false unless zip > 9999 && zip < 100000}
 
     expect(all_five_digits).to be false
   end
 
-  xit 'test 9' do
+  it 'test 9' do
     snacks = ["GARLIC PLANTAINS", "SNICKERDOODLES", "Pretzels"]
-    # Your Code Here
+    all_caps = true
+    snacks.each {|snack| all_caps = false if snack == snack.upcase}
 
-    assert_equal false, all_caps
     expect(all_caps).to be false
   end
 
-  xit 'test 10' do
+  it 'test 10' do
     snacks = {
       savory: "GARLIC PLANTAINS",
       sweet: "SNICKERDOODLES",
       salty: "Pretzels"
     }
-    # Your Code Here
+    all_caps = true 
+    snacks.each {|k, snack| all_caps = false if snack == snack.upcase }
 
     expect(all_caps).to be false
   end
